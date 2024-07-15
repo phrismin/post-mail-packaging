eba3ef183e6666e3b9ceea7c9ae43a23dbe809f9
rudoy <rudenw01@gmail.com>
2024-07-15 23:03:02 +0300

    first commit

---
+++ smtp-mail-service/src/main/java/com/example/smpt_service/SmptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.smpt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmptServiceApplication.class, args);
+	}
+
+}
---
+++ smtp-mail-service/src/test/java/com/example/smpt_service/SmptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smpt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
