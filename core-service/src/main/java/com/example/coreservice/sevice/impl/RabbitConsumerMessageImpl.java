ada9684ef3033db12f0b67862b9d64dc7b12813c
rudoy <rudenw01@gmail.com>
2024-07-18 23:04:57 +0300

    11

--- core-service/src/main/java/com/example/coreservice/sevice/impl/RabbitConsumerMessageImpl.java
+++ core-service/src/main/java/com/example/coreservice/sevice/impl/RabbitConsumerMessageImpl.java
@@ -6,7 +6,10 @@ import com.example.coreservice.sevice.MessageService;
 import com.example.coreservice.sevice.RabbitConsumerMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,19 +18,31 @@ import org.springframework.stereotype.Service;
 public class RabbitConsumerMessageImpl implements RabbitConsumerMessageService {
 
     private final MessageService messageService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     @Override
     @RabbitListener(queues = "message.process")
     public void processMessage(MessageDto messageDto) {
         log.info("Получено на обработку в core-service:{}", messageDto.toString());
         String template = messageService.findTemplateById(messageDto.getTemplateId());
-//        Integer groupUsers = messageDto.getGroupUsers();
+        Integer groupUsers = messageDto.getGroupUsers();
 
-        sendMessageToSmtpService();
+        String message = "test";
+        sendMessageToSmtpService(message);
     }
 
-    private void sendMessageToSmtpService() {
-        log.info("Письмо отправлено на обработку в smtp-mail-service:{}");
+    private void sendMessageToSmtpService(String message) {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
+        rabbitTemplate.getMessageConverter().toMessage(message, messageProperties);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+        log.info("Письмо отправлено на обработку в smtp-mail-service:{}", message);
     }
 
 
